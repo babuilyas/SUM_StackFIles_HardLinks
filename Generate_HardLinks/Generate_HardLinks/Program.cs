68b8a396b525fe982610b98637ae6af2fed6b31c
Muhammad Ilyas <ilyas.com.pk@gmail.com>
2019-10-16 12:43:35 +0500

    stackfiles.txt is replaced with original xls file download from maintenance planner.

--- Generate_HardLinks/Generate_HardLinks/Program.cs
+++ Generate_HardLinks/Generate_HardLinks/Program.cs
@@ -10,6 +10,49 @@ namespace Generate_HardLinks
     {
         static void Main(string[] args)
         {
+            
+
+            var xDocument = System.Xml.Linq.XDocument.Load("stackfiles.xls");
+            // XML elements are in the "ss" namespace.
+            System.Xml.Linq.XNamespace ss = "urn:schemas-microsoft-com:office:spreadsheet";
+            var rows = xDocument.Root.Element(ss + "Worksheet")
+                            .Element(ss + "Table").Elements(ss + "Row");
+            var files = from c in rows                        
+                        select c.Elements(ss + "Cell").ElementAt(1).Element(ss + "Data").Value;
+            var notfound = new List<string>();
+            int i = 1;
+            for (var j = 5; j < files.Count(); j++)
+            {
+                var file = files.ElementAt(j);
+                Console.WriteLine("{0}/{1} {2}", i++, files.Count() - 5, file);
+                var xfiles = new List<System.IO.FileInfo>();
+                if (file.Length > 0)
+                {
+                    foreach (var dir in new System.IO.DirectoryInfo(Environment.CurrentDirectory).Root.GetDirectories())
+                    {
+                        try
+                        {
+                            var xfiles2 = dir.GetFiles(new System.IO.FileInfo(file).Name, System.IO.SearchOption.AllDirectories);
+                            xfiles.AddRange(xfiles2.ToList<System.IO.FileInfo>());
+                        }
+                        catch { continue; }
+                    }
+                }
+                if (xfiles.Count > 0)
+                    CreateHardLink(Environment.CurrentDirectory + "\\" + file, xfiles[0].FullName, IntPtr.Zero);
+                else
+                    notfound.Add(file);
+            }
+            if (notfound.Count > 0)
+            {
+                var txt = System.IO.File.CreateText("notfound.txt");
+                foreach (var file in notfound)
+                    txt.WriteLine(file);
+                txt.Close();
+            }
+        }
+        static void Main2(string[] args)
+        {
             var files = System.IO.File.ReadAllLines("stackfiles.txt");
             var notfound = new List<string>();
             int i = 1;
